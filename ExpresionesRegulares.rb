1fd35ea4fb73b115d74e635ed2870c8cb634b908
codeamac03 <codeamac03@gmail.com>
2016-06-16 09:34:01 -0500

    Cambio de Expresiones

--- ExpresionesRegulares.rb
+++ ExpresionesRegulares.rb
@@ -38,44 +38,44 @@
 #  menos dígitos no debería remplazarlo.
 
 ##################################################################################################################################
-#  Regresa true si encuentra un numero de cuenta y numero no valido si no lo hace
-#  def encontrar_number(string)
-#     if string.match(/(\d{4})-(\d{3})-(\d{3})$/)  
-#      true
-#    else
-#      "Numero no valido"
-#    end
-#  end
+#Regresa true si encuentra un numero de cuenta y numero no valido si no lo hace
+ def encontrar_number(string)
+    if string.match(/(\d{4})-(\d{3})-(\d{3})$/)  
+     true
+   else
+     "Numero no valido"
+   end
+ end
 
 
-# # #Regresa un numero de cuenta si exite dentro del string y nil en el caso contrario.
-# def existencia_number(string)
-#    if /(\d{4})-(\d{3})-(\d{3})$/ =~ string 
-#      p string 
-#    else
-#      nil
-#    end
-#  end
+#Regresa un numero de cuenta si exite dentro del string y nil en el caso contrario.
+def existencia_number(string)
+   if /(\d{4})-(\d{3})-(\d{3})$/ =~ string 
+     p string 
+   else
+     nil
+   end
+ end
 
-# #Regresa un array con los números de cuenta que existen dentro del string y un array vacío en el caso contrario.
-# def numeros_cuenta(string)
-#   array = []
-#   if string.include?(string)
-#     array << /\d{4}\-\d{3}-\d{3}/.match(string).to_s
-#   else
-#     array
-#   end
-# end
+#Regresa un array con los números de cuenta que existen dentro del string y un array vacío en el caso contrario.
+def numeros_cuenta(string)
+  array = []
+  if string.include?(string)
+    array << /\d{4}\-\d{3}-\d{3}/.match(string).to_s
+  else
+    array
+  end
+end
 
 # #Regresa un string donde si existen números de cuenta estos tendran remplazados por "X" 
-# #los primeros siete numeros. ej. "XXXX-XXX-234"
-# def remplazo(string)
-#       if string.include?(string)
-#            string.gsub(/\d{4}\-\d{3}/, "XXXX-XXX")
-#       else
-#         puts "numero no encontrado"
-#   end
-# end
+#los primeros siete numeros. ej. "XXXX-XXX-234"
+def remplazo(string)
+      if string.include?(string)
+           string.gsub(/\d{4}\-\d{3}/, "XXXX-XXX")
+      else
+        puts "numero no encontrado"
+  end
+end
 
 #Regresa un string formateado donde cualquier número de diez dígitos seguido o si tiene puntos en vez 
 #de guiones lo regresa a su formato original donde usa guiones para dividir los diez dígitos. 
@@ -89,31 +89,33 @@ def simbolos_cambiados(string)
     string
   end  
 end
+
+
 # Drive code encontrar_number
-# p encontrar_number ("1234-123-123") 
-# p encontrar_number ("1214-823-1a5") 
-# p encontrar_number ("9898-aaa-eee")
-# p encontrar_number ("lle4-777-ewe")
+p encontrar_number ("1234-123-123") 
+p encontrar_number ("1214-823-1a5") 
+p encontrar_number ("9898-aaa-eee")
+p encontrar_number ("lle4-777-ewe")
 
-# # #Drive code de existencia_number
-# p existencia_number ("1234-123-123")
-# p existencia_number ("12a4-823-1a5") 
-# p existencia_number ("9898-aaa-eee")
-# p existencia_number ("lle4-777-ewe")
+#Drive code de existencia_number
+p existencia_number ("1234-123-123")
+p existencia_number ("12a4-823-1a5") 
+p existencia_number ("9898-aaa-eee")
+p existencia_number ("lle4-777-ewe")
 
-# # Drive code de numeros_cuenta
-# p numeros_cuenta ("1234-123-123")
-# p numeros_cuenta ("lle4-777-ewa")
-# p numeros_cuenta ("lla4-7w7-rae")
-# p numeros_cuenta ("qwew-799-qwe")
+# Drive code de numeros_cuenta
+p numeros_cuenta ("1234-123-123")
+p numeros_cuenta ("lle4-777-ewa")
+p numeros_cuenta ("lla4-7w7-rae")
+p numeros_cuenta ("qwew-799-qwe")
 
 # #Drive code de remplazo
-# p remplazo ("1234-123-123")
-# p remplazo ("1234-777-aqe")
-# p remplazo ("1234-727-twe")
-# p remplazo ("1234-799-par")
+p remplazo ("1234-123-123")
+p remplazo ("1234-777-aqe")
+p remplazo ("1234-727-twe")
+p remplazo ("1234-799-par")
 
-#Drive code de simbolos_cambiados
+# #Drive code de simbolos_cambiados
 p simbolos_cambiados ("1234-123-123")
 p simbolos_cambiados ("1234.123.123")
 p simbolos_cambiados ("1234123123")
